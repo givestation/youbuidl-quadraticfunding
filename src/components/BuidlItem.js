4fa9ac9e4c0e1ad12d2370c3c0979634c7fc13ec
0xmikki <90579247+MrBloomguy@users.noreply.github.com>
2024-01-25 12:32:12 +0000

    newnew

--- src/components/BuidlItem.js
+++ src/components/BuidlItem.js
@@ -53,18 +53,7 @@ const BuidlItem = ({ project, tag }) => {
               >
                 {project?.creator?.slice(0, 15)}...
               </a>
-              <svg
-                width='17'
-                height='17'
-                viewBox='0 0 17 17'
-                fill='none'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <path
-                  d='M15.2717 7.60764L14.3083 6.48848C14.1242 6.27598 13.9754 5.87931 13.9754 5.59598V4.39181C13.9754 3.64098 13.3592 3.02473 12.6083 3.02473H11.4042C11.1279 3.02473 10.7242 2.87598 10.5117 2.69181L9.3925 1.72848C8.90375 1.31056 8.10334 1.31056 7.6075 1.72848L6.49542 2.69889C6.28292 2.87598 5.87917 3.02473 5.60292 3.02473H4.3775C3.62667 3.02473 3.01042 3.64098 3.01042 4.39181V5.60306C3.01042 5.87931 2.86167 6.27598 2.68459 6.48848L1.72834 7.61473C1.3175 8.10348 1.3175 8.89681 1.72834 9.38556L2.68459 10.5118C2.86167 10.7243 3.01042 11.121 3.01042 11.3972V12.6085C3.01042 13.3593 3.62667 13.9756 4.3775 13.9756H5.60292C5.87917 13.9756 6.28292 14.1243 6.49542 14.3085L7.61459 15.2718C8.10334 15.6897 8.90375 15.6897 9.39959 15.2718L10.5188 14.3085C10.7313 14.1243 11.1279 13.9756 11.4113 13.9756H12.6154C13.3663 13.9756 13.9825 13.3593 13.9825 12.6085V11.4043C13.9825 11.1281 14.1313 10.7243 14.3154 10.5118L15.2788 9.39264C15.6896 8.90389 15.6896 8.09639 15.2717 7.60764ZM11.4467 7.16139L8.02542 10.5826C7.92625 10.6818 7.79167 10.7385 7.65 10.7385C7.50834 10.7385 7.37375 10.6818 7.27459 10.5826L5.56042 8.86848C5.355 8.66306 5.355 8.32306 5.56042 8.11764C5.76584 7.91223 6.10584 7.91223 6.31125 8.11764L7.65 9.45639L10.6958 6.41056C10.9013 6.20514 11.2413 6.20514 11.4467 6.41056C11.6521 6.61598 11.6521 6.95598 11.4467 7.16139Z'
-                  fill='#74D12A'
-                />
-              </svg>
+              
                 </div>
 
 
@@ -106,10 +95,8 @@ const BuidlItem = ({ project, tag }) => {
               <div style={{color:"#3EA7E1"}}>Popular</div>
             </div>
             <p className='text-Nickle font-normal text-sm mt-5 flex flex-col'>
-              {project?.desc?.slice(0, 50)} ...
-              <span className='text-Vampire-Black cursor-pointer underline'>
-                Read more
-              </span>
+              {project?.desc?.slice(0, 100)} ...
+              
               {/* {desc.length > 1000 && (
               <span className='text-Vampire-Black cursor-pointer'>
                 Read more
@@ -121,15 +108,15 @@ const BuidlItem = ({ project, tag }) => {
            
             <div className='flex items-center justify-between'>
               <h3 className=' font-normal text-xs flex items-center gap-0.5'>
-              <div style={{color:"#818283",background:"#DADFE2"}} className="bg-gray-400 rounded p-0.5">Currently raising</div>
+              <div style={{color:"#818283",background:"#DADFE2"}} className="bg-gray-400 rounded p-0.4">Currently raising</div>
                 <div  className='text-Vampire-Black mt-1'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 7 7" fill="none">
+                <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 7 7" fill="none">
 <ellipse cx="3.43124" cy="3.01465" rx="3.27187" ry="3.01465" fill="#12D69B"/>
 </svg>
                 </div>
               </h3>
               <h3 className=' font-normal text-xs'>
-              <div style={{color:"#818283",background:"#DADFE2"}} className="bg-gray-400 rounded p-0.5">QF Matching</div>
+              <div style={{color:"#818283",background:"#DADFE2"}} className="bg-gray-400 rounded p-0.4">QF Matching</div>
               </h3>
             </div>
           </div>
@@ -151,7 +138,7 @@ const BuidlItem = ({ project, tag }) => {
             </div>
           </div>
           <div className='flex items-center justify-between'>
-          <div style={{color:"#818283",background:"#DADFE2"}} className="bg-gray-400 rounded p-0.5 text-xs" >Target  ${formatUnits?.(project?.goalAmount === undefined ? 0 : (project?.goalAmount), (project?.chainId == 56 || project?.chainId == 1 ? 18 : 6))}</div>
+          <div></div>
 
             {!isConnected && (
               <Account />
