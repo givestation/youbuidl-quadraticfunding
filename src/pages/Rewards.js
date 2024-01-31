1ae096783c215ffaa4ffdf90fe5efb6cead3e79c
0xmikki <90579247+MrBloomguy@users.noreply.github.com>
2024-01-31 08:51:51 +0100

    Update Rewards.js

--- src/pages/Rewards.js
+++ src/pages/Rewards.js
@@ -155,7 +155,7 @@ const Rewards = () => {
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
                 <th scope="col" class="px-6 py-3">
-                    Users
+                    Rank
                 </th>
                 <th scope="col" class="px-6 py-3">
                     Contributions
